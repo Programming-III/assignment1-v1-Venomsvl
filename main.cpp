a7b0c5694339123cc41290464c438c8be7d0ef99
Lana Hatem <Lana.elgayed@student.giu-uni.de>
2025-11-12 15:18:32 +0200

    Refactor includes and correct animal class instantiation
    Fixed include directives and corrected class names.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
+#include <iostream>
 #include "Animal.h"
+#include "Mammel.h"
 #include "Bird.h"
-#include "Mammal.h"
 #include "Reptile.h"
 #include "Enclosure.h"
 #include "Visitor.h"
-#include <iostream>
+
 using namespace std;
 
-
-// ============== MAIN FUNCTION ==============
 int main() {
-    
-    
+
+    AnimalMammel M1("Lion",5,"Hungry");
+    AnimalBird B1("Parrot",5,"Not Hungry");
+    AnimalnimalReptile R1("Snake",3, "Hungry");
+
+    M1.addAnimal(M1);
+    B1.addAnimal(B1);
+    R1.addAnimal(R1);
+
+    Visitor V1("Sarah Ali",3);
+
     return 0;
 }
