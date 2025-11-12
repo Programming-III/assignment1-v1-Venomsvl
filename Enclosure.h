3790c6eb8e5ec3e389675444b881d213cb847571
Lana Hatem <Lana.elgayed@student.giu-uni.de>
2025-11-12 15:11:52 +0200

    Refactor Enclosure.h with new header guards and methods
    Updated header guards and added methods to the Enclosure class.

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,25 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
+#ifndef ASSIGNMENT1_ENCLOSURE_H
+#define ASSIGNMENT1_ENCLOSURE_H
+#include<iostream>
 
 #include "Animal.h"
+using namespace std;
 
-//define enclosure class here 
+class Enclosure {
+private:
+     Animal* animal;
+    int Capacity;
+    int currentCount;
+public:
+    void displayAnimal() const {
+        for(int i=0;i<Capacity;i++) {
+            cout<<ainmal[i]<<" ";
+        }
+    }
 
-
-
-
-
-
-
-
-#endif
+     void addAnimal(Animal* animal);
+    Enclosure();
+        Enclosure(string name,int age);
+    ~Enclosure();
+};
+#endif //ASSIGNMENT1_ENCLOSURE_H
