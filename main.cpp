0566c2e26f1c66734a4ee48b12c6602088db363b
Lana Hatem <Lana.elgayed@student.giu-uni.de>
2025-11-12 15:27:14 +0200

    Fix animal initialization and add to enclosure
    Updated animal instances and added them to an enclosure.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,22 @@
 #include "Reptile.h"
 #include "Enclosure.h"
 #include "Visitor.h"
-
 using namespace std;
 
 int main() {
 
     AnimalMammel M1("Lion",5,"Hungry");
-    AnimalBird B1("Parrot",5,"Not Hungry");
-    AnimalnimalReptile R1("Snake",3, "Hungry");
+    AnimalBird B1("Parrot",2,"Not Hungry");
+    AnimalReptile R1("Snake",3,"Hungry");
 
-    M1.addAnimal(M1);
-    B1.addAnimal(B1);
-    R1.addAnimal(R1);
+    Enclosure E1;
+    E1.addAnimal(&M1);
+    E1.addAnimal(&B1);
+    E1.addAnimal(&R1);
 
     Visitor V1("Sarah Ali",3);
+
+    E1.displayAnimal();
 
     return 0;
 }
